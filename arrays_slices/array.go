0e536454124d532428d92226fca29d4800d4a169
Fernando Cordero <fernando.corderobastidas@cencosud.cl>
2023-10-21 01:09:23 -0300

    ADD: array - matrix

--- arrays_slices/array.go
+++ arrays_slices/array.go
@@ -3,6 +3,7 @@ package arrays_slices
 import "fmt"
 
 var table [10]int = [10]int{1, 2, 3, 4, 5}
+var matrix [8][8]int
 
 func ShowArray() {
 	table[7] = 33
@@ -11,4 +12,11 @@ func ShowArray() {
 	table2 := [10]string{"Paul", "Dan"}
 	fmt.Println(table)
 	fmt.Println(table2)
+
+	for i := 0; i < len(table); i++ {
+		fmt.Println(table[i], "<<")
+	}
+
+	matrix[7][2] = 15
+	fmt.Println(matrix)
 }
