3d08dbed7df383befb2fb71d25f03a9a7a1a2e00
Fernando Cordero <fernando.corderobastidas@cencosud.cl>
2023-10-21 01:03:52 -0300

    ADD: array

--- arrays_slices/array.go
+++ arrays_slices/array.go
@@ -2,11 +2,10 @@ package arrays_slices
 
 import "fmt"
 
-var table [10]int
+var table [10]int = [10]int{1, 2, 3, 4, 5}
 
 func ShowArray() {
 	table[7] = 33
 	table[9] = 100
-
 	fmt.Println(table)
 }
